00b7f042c3dbdd8373f2bad98d448c24f71b40c5
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-09-20 00:24:36 +0100

    Converting deck type to slice of strings to a comma separated string.

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of 'deck
 // which is a slice of strings
@@ -35,4 +38,9 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// takes a deck and makes it a string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
