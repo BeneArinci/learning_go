d5119d5a7fc8248a8d404292b7870d36d39962b3
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-09-20 00:32:05 +0100

    Write to file.

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -43,4 +44,8 @@ func (d deck) print() {
 // takes a deck and makes it a string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
