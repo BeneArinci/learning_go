834823bce16f0bbb6958aa93cd00d7b937f04281
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-09-22 22:12:58 +0100

    Shuffle deck. Rand function management.

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck
@@ -63,4 +65,14 @@ func newDeckFromFile(filename string) deck {
 
 	return d
 
+}
+
+func (d deck) shuffleDeck() {
+	for i := range d {
+		// generate a random seed for rand function
+		rand.Seed(time.Now().UnixNano())
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	cards := newDeckFromFile("my_cards")
+	cards.shuffleDeck()
 	cards.print()
 }
