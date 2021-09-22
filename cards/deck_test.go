69d4f77a4c3b6bd7e54873d1a7a570f2d44cbbfb
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-09-22 22:52:16 +0100

    Testing funcs that use files.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,30 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected deck first item to be King of Clubs instead received %v", d[len(d)-1])
 	}
+}
+
+// when dealing with file creation we need to make sure we get rid of the file we create
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 40 {
+		t.Errorf("Expected deck length of 40 but got %v", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != "Ace of Spades" {
+		t.Errorf("Expected deck first item to be Ace of Spades instead received %v", d[0])
+	}
+
+	if loadedDeck[len(loadedDeck)-1] != "King of Clubs" {
+		t.Errorf("Expected deck first item to be King of Clubs instead received %v", d[len(loadedDeck)-1])
+	}
+
+	os.Remove("_decktesting")
+
 }
