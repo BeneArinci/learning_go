8c0dc4bfcd59e3f6bef1faf54a660f37311f6e7e
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-10-02 16:00:53 +0100

    The http package and its complex interface structure.

---
+++ interfaces/http-package/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+	if err != nil {
+		println("Error: ", err)
+		os.Exit(1)
+	}
+
+	// The empty byte slice we create is quite big because
+	// the Read func doesn't have the ability to resize it
+	// and we do not want to end up with an error
+	bs := make([]byte, 99999)
+
+	// The Read func is applicable to any type of element.
+	// It takes whatever is coming from the receiver into the empty []byte
+	// that is passed as argument.
+	resp.Body.Read(bs)
+
+	fmt.Println(string(bs))
+}
