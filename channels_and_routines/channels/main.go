86c3b00a3456638d7cb82a5203095544def827a3
Benedetta Arinci <benedetta.arinci@gmail.com>
2021-10-03 16:47:18 +0100

    Channels and Go routines. Making multiple http req.

---
+++ channels_and_routines/channels/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{"http://google.com", "http://amazon.com", "http://golang.org", "http://facebook.com", "http://stackoverflow.com"}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, " might be down!")
+		return
+	}
+	fmt.Println(link, " is up!")
+}
